4cfe57359d41ebf0d67db1b55c8c9d524f29d529
CommanderStone <commander.stone@gmx.de>
2020-04-01 20:07:55 +0200

    a

--- index.php
+++ index.php
@@ -1,11 +1,16 @@
 <!DOCTYPE html>
 <html lang="en">
 <head>
+    <link rel="stylesheet" type="text/css" href="mystyle.css">
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Document</title>
 </head>
 <body>
-    
+    <div class="rahmen">
+        <div class="info-top">Hier kommen ganz viele Infos rein.
+
+        </div>
+    </div>
 </body>
 </html>
